f85c091739707a3e7e1a2fcaa7176e3c11667609
Asta Havshoj <astah2000@gmail.com>
2024-11-20 16:58:03 +0100

    Endelig udgave

--- js/spil.js
+++ js/spil.js
@@ -1,62 +1,66 @@
+// VARIABLER
 let point;
 let liv;
 
+// START APLIKATIONEN NÅR SIDEN VISES
 window.addEventListener("load", sidenVises);
+
+// KONSTANTER
 const max = 6;
 const godOst = document.querySelector("#god_ost_container");
 const mugOst = document.querySelector("#mug_ost_container");
-
 const godOst2 = document.querySelector("#god_ost_container2");
 const mugOst2 = document.querySelector("#mug_ost_container2");
 
 function sidenVises() {
   console.log("SidenVises");
-  // TAG STILLING TIL, HVAD DER SKAL SKE MED DENNE SANG
-  // document.querySelector("#forside").volume = 0.3;
-  // document.querySelector("#forside").play();
 
-  // Skjul andre skærme
-  //   Vis start skærm
-  //   Gør start-knap klikbar, klik på start/play
+  // AFSPIL BAGGRUNDSMUSIK
+  document.querySelector("#forside_lyd").volume = 0.3;
+  document.querySelector("#forside_lyd").play();
 
-  // startSpillet();
-
-  // afspil baggrundsmusik
-  // document.querySelector("#musik").volume = 0.3;
-  // document.querySelector("#musik").play();
-
+  //   VIS STARTSKÆRM
+  // SKJUL ANDRE SKÆRME
   document.querySelector("#game_over").classList.add("hide");
   document.querySelector("#level_complete").classList.add("hide");
+
+  //   GØR START-KNAP KLIKBAR, KLIK PÅ START/PLAY
   document.querySelector("#play_knap").addEventListener("click", startSpillet);
 }
-
+// GENERERER ET TILFÆLDIGT TAL FRA 1 TIL MAX
 function nytRand(max) {
   return Math.floor(Math.random() * max) + 1;
 }
 
 function startSpillet() {
-  // afspil baggrundsmusik
+  console.log("startSpillet");
+
+  //  STOP INTROMUSIK
+  document.querySelector("#forside_lyd").pause();
+
+  // AFSPIL BAGGRUNDSMUSIK
   document.querySelector("#musik").volume = 0.3;
   document.querySelector("#musik").play();
-  console.log("startSpillet");
-  // skjul andre skærme
 
-  //Nulstil point og liv
+  // SKJUL ANDRE SKÆRME
+
+  // NULSTIL POINT OG LIV
   point = 0;
   console.log(point);
   document.querySelector("#score_board").innerHTML = point;
-  //  Skriv point og liv ud
+
+  //  SKRIV POINT OG LIV UD
   liv = 3;
   console.log(liv);
   document.querySelector("#life_board").innerHTML = liv;
 
-  //   Start timer -animation
+  //   START TIMER-ANIMATION
   document.querySelector("#minut_viser").classList.add("minut_animation");
   document.querySelector("#time_viser").classList.add("time_animation");
   document.querySelector("#time_board").addEventListener("animationend", stopSpillet);
 
-  //   Random position og random delay
-  //   Start fald -animationer på elementer
+  //   RANDOM POSITION OG RANDOM DELAY
+  //   START FALD-ANIMATIONER PÅ ELEMENTER
   godOst.classList.add("fald", "delay" + nytRand(5));
   godOst2.classList.add("fald", "delay" + nytRand(5));
   godOst.classList.add("pos" + nytRand(6));
@@ -67,28 +71,26 @@ function startSpillet() {
   mugOst.classList.add("pos2");
   mugOst2.classList.add("pos2");
 
-  // console.log("her er mit random delay", randomDelay);
-
-  //   lyt efter at faldanimtion på gul ost har kørt én gang
-  //   lyt efter at faldanimtion på grøn ost har kørt én gang
+  //   LYT EFTER AT FALDANIMATION PÅ GUL HAR KØRT EN GANG
   godOst.addEventListener("animationiteration", goodReset);
   godOst2.addEventListener("animationiteration", goodReset);
 
+  //  LYT EFTER AT FALDANIMATION PÅ GRØN HAR KØRT EN GANG
   mugOst.addEventListener("animationiteration", badReset);
   mugOst2.addEventListener("animationiteration", badReset);
 
-  //   Gør gul ost klikbart
-  //   gør grøn ost klikbart
+  //   GØR GRØN OST KLIKBAR
   mugOst.addEventListener("click", clickBad);
   mugOst2.addEventListener("click", clickBad);
 
+  //   GØR GUL OST KLIKBAR
   godOst.addEventListener("click", clickGood);
   godOst2.addEventListener("click", clickGood);
 
-  //   Lyt efter om timeranimation er færdig
+  //  LYT EFTER OM TIMERANIMATION ER FÆRDIG
   document.querySelector("#minut_viser").addEventListener("animationend", stopSpillet);
 
-  // HIDE ANDRE SKRÆRME OG GAME OVER
+  // SKJUL ANDRE SKRÆRME OG GAME OVER
   document.querySelector("#game_over").classList.add("hide");
   document.querySelector("#igen_mug").classList.remove("hide");
 
@@ -97,65 +99,67 @@ function startSpillet() {
 
   document.querySelector("#start").classList.add("hide");
 }
-
-// ændrer alle god og dårlig til this, fordi jeg har fået flere elementer
+// FRYS ANIMATION PÅ KLIK
 function clickGood() {
   console.log("clickGood");
   this.classList.add("frys");
 
-  //   Få 1 point
+  //   FÅ 1 POINT
   point++;
   document.querySelector("#score_board").textContent = point;
   //   Skriv point ud
 
-  //   Afspil lyd good
+  //   AFSPIL GOD_LYD
   document.querySelector("#god_lyd").volume = 1;
   document.querySelector("#god_lyd").currentTime = 0;
   document.querySelector("#god_lyd").play();
 
-  //   Start goodForsvind -animation
+  //   START GOODFORSVIND-ANIMATION
   this.firstElementChild.classList.add("forsvind_good");
-  //   lyt efter goodForsvind-animation er færdig
+
+  //   LYT EFTER GOODFORSVIND-ANIMATION ER FÆRDIG
   this.addEventListener("animationend", goodReset);
 
-  //fjern event listener
+  //FJERN EVENTLISTENER
   this.removeEventListener("click", clickGood);
 }
 
+// NULSTIL ELEMENT
 function goodReset() {
   console.log("goodReset");
   this.classList = "";
   this.firstElementChild.classList = "";
 
-  // Vis element igen
-  //   Ny random position
-
+  // VIS ELEMENT IGEN
+  // NY RANDOM POSITION
   this.classList.add("pos" + nytRand(6));
-  //   Genstart fald -animation
+
+  // GENSTART FALD-ANIMATION
   this.offsetLeft;
   this.classList.add("fald");
 
-  //add eventlistener igen
+  //TILFØJ EVENTLISTENER IGEN
   this.addEventListener("click", clickGood);
 }
 
 function clickBad() {
   console.log("clickBad");
 
-  //   Afspil lyd bad
+  //   AFSPIL LYD MUG_LYD
   document.querySelector("#mug_lyd").volume = 0.5;
   document.querySelector("#mug_lyd").currentTime = 0;
   document.querySelector("#mug_lyd").play();
 
-  //   badForsvind -animation
+  //  START BADFORSVIND-ANIMATION
   this.firstElementChild.classList.add("forsvind_bad");
-  //   lyt efter badForsvind-animation er færdig
+
+  //   LYT EFTER OM BADFORSVIND-ANIMATION ER FÆRDIG
   this.addEventListener("animationend", badReset);
 
-  //fjern event listener
+  //FJERN EVENTLISTENER FOR KLIK
   this.removeEventListener("click", clickBad);
 
-  // mist et liv
+  // MIST ET LIV
   liv--;
 
   //TJEK OM DER ER MERE END NUL LIV
@@ -163,36 +167,38 @@ function clickBad() {
     stopSpillet();
   }
 
-  //   Vis antal liv
+  // VIS ANTAL LIV
   document.querySelector("#life_board").textContent = liv;
 }
 
+// NULSTIL ELEMENT
 function badReset() {
   console.log("badReset");
   this.classList = "";
   this.firstElementChild.classList = "";
 
-  // Vis element igen
-  //   Ny random position
+  // VIS ELEMENT IGEN
+  // NY RANDOM POSITION
   let myRand = Math.floor(Math.random() * max) + 1;
   this.classList.add("pos" + myRand);
-  //   Genstart fald -animation
+
+  //   GENSTART FALD-ANIMATION
   this.offsetLeft;
   this.classList.add("fald");
 
-  //add eventlistener igen
+  //TILFØJ EVENTLISTENER IGEN
   this.addEventListener("click", clickBad);
 }
 
 function stopSpillet() {
   console.log("stopSpillet");
 
-  // stop timer
+  // STOP TIMER
   document.querySelector("#minut_viser").classList = "";
   document.querySelector("#time_viser").classList = "";
   document.querySelector("#time_board").removeEventListener("animationend", stopSpillet);
 
-  // god
+  // NULSTIL ANIMATIONER OG EVENTLISTENERS FOR GUL OST
   godOst.classList = "";
   godOst2.classList = "";
   godOst.firstElementChild.classList = "";
@@ -207,7 +213,7 @@ function stopSpillet() {
   godOst.removeEventListener("mousedown", clickGood);
   godOst2.removeEventListener("mousedown", clickGood);
 
-  // Dårlig
+  // NULSTIL ANIMATIONER OG EVENTLISTENERS FOR GRØN OST
   mugOst.classList = "";
   mugOst2.classList = "";
   mugOst.firstElementChild.classList = "";
@@ -222,6 +228,8 @@ function stopSpillet() {
   mugOst.removeEventListener("mousedown", clickBad);
   mugOst2.removeEventListener("mousedown", clickBad);
 
+  // TJEK SPILSTATUS (OM JEG HAR MERE END 15 POINT)
+  // TJEK OM JEG HAR LIV TILBAGE
   if (liv <= 0) {
     console.log("Du har tabt");
     gameOver();
@@ -232,40 +240,36 @@ function stopSpillet() {
     console.log("Du har tabt");
     gameOver();
   }
-
-  //Fjern alle animationer
-  //   Fjern alle eventListerner-ere
-  //   tjek om jeg har mere end 20 point
-  // tjek om jeg har liv tilbage
 }
-
+//   VIS GAME-OVER SKÆRM
 function gameOver() {
   console.log("gameOver");
-  //   Vis gameover skærm
-  //   gør "spil igen" klikbart
+
+  //   GØR "SPIL IGEN" KLIKBAR
   document.querySelector("#game_over").classList.remove("hide");
   document.querySelector("#igen_mug").addEventListener("click", startSpillet);
 
+  // AFSPIL BAGGRUNDSMUSIK
   document.querySelector("#musik").volume = 0.1;
   document.querySelector("#musik").play();
 
+  // AFSPIL LYD FOR GAME-OVER
   document.querySelector("#over_lyd").volume = 1;
   document.querySelector("#over_lyd").play();
 }
 
+// VIS LEVEL-COMPLETE SKÆRM
 function levelComplete() {
-  // baggrundsmusik
+  // AFSPIL BAGGRUNDSMUSIK
   document.querySelector("#musik").volume = 0.1;
   document.querySelector("#musik").play();
 
+  // AFSPIL LUD FOR LEVEL COMPLETE
   document.querySelector("#complete_lyd").volume = 1;
   document.querySelector("#complete_lyd").play();
 
+  //   GØR SPIL IGEN KLIKBART
   console.log("levelComplete");
   document.querySelector("#level_complete").classList.remove("hide");
-
   document.querySelector("#igen_gul").addEventListener("click", startSpillet);
-
-  //   Vis levelcomplete skærm
-  //   gør "spil igen" klikbart
 }
